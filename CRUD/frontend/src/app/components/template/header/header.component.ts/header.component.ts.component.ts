546198007a5ca77462945f548db175b70c0dc3ea
Paulo França <paulovinicius.franca@gmail.com>
2022-04-14 11:11:24 -0300

    Instala Material e cria headerComponent

---
+++ CRUD/frontend/src/app/components/template/header/header.component.ts/header.component.ts.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-header.component.ts',
+  templateUrl: './header.component.ts.component.html',
+  styleUrls: ['./header.component.ts.component.css']
+})
+export class HeaderComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
